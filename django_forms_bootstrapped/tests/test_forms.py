eef0829f088235bb707797fb34fcba17c9f1ecf1
Bruno Miguel Silva <bmsilva@gmail.com>
2014-04-02 15:02:37 +0100

    Testing ChoiceField

--- django_forms_bootstrapped/tests/test_forms.py
+++ django_forms_bootstrapped/tests/test_forms.py
@@ -27,7 +27,7 @@ class FormTestCase(TestCase):
             u'<input class="form-control" id="id_name" maxlength="50" name="name" type="text" />'
             u'</p>'
         )
-        self.assertEqual(rendered, html)
+        self.assertEqual(rendered, html, 'testing simple form with no bootstrap')
 
     def test_simple_form_bootstrap_options(self):
         class SimpleForm(forms.Form):
@@ -44,7 +44,7 @@ class FormTestCase(TestCase):
             u'<input class="form-control" id="id_name" maxlength="50" name="name" type="text" />'
             u'</div>'
         )
-        self.assertEqual(rendered, html)
+        self.assertEqual(rendered, html, 'testing simple form with bootstrap options')
 
         SimpleForm.bootstrap_options['form_layout'] = 'form-horizontal'
         sform = SimpleForm()
@@ -57,7 +57,20 @@ class FormTestCase(TestCase):
             u'</div>'
             u'</div>'
         )
-        self.assertEqual(rendered, html)
+        self.assertEqual(rendered, html, 'testing simple form_layout')
+
+        SimpleForm.bootstrap_options['form_layout_sizes'] = (4, 6)
+        sform = SimpleForm()
+        rendered = sform.as_p()
+        html = (
+            u'<div class="form-group">'
+            u'<label class="control-label col-sm-4" for="id_name">Name:</label> '
+            u'<div class="col-sm-6">'
+            u'<input class="form-control" id="id_name" maxlength="50" name="name" type="text" />'
+            u'</div>'
+            u'</div>'
+        )
+        self.assertEqual(rendered, html, 'Testing simple form_layout_sizes')
 
 
     def test_charfield(self):
@@ -71,5 +84,54 @@ class FormTestCase(TestCase):
             u'<label class="control-label" for="id_body">Body:</label> '
             u'<textarea class="form-control" cols="40" id="id_body" name="body" rows="10">\r\n</textarea>'
             u'</p>'
+        )
+        self.assertEqual(rendered, html)
+
+    def test_choicefield(self):
+        class SimpleForm(forms.Form):
+            choose = forms.ChoiceField(
+                choices=((1, 'choice 1'), (2, 'choice 2')))
+            bootstrap_options = {}
+
+        sform = SimpleForm()
+        rendered = sform.as_p()
+        html = (
+            u'<p class="form-group">'
+            u'<label class="control-label" for="id_choose">Choose:</label> '
+            u'<select class="form-control" id="id_choose" name="choose">\n'
+            u'<option value="1">choice 1</option>\n'
+            u'<option value="2">choice 2</option>\n'
+            u'</select>'
+            u'</p>'
+        )
+        self.assertEqual(rendered, html)
+
+        SimpleForm.bootstrap_options['as_p_use_divs'] = True
+        sform = SimpleForm()
+        rendered = sform.as_p()
+        html = (
+            u'<div class="form-group">'
+            u'<label class="control-label" for="id_choose">Choose:</label> '
+            u'<select class="form-control" id="id_choose" name="choose">\n'
+            u'<option value="1">choice 1</option>\n'
+            u'<option value="2">choice 2</option>\n'
+            u'</select>'
+            u'</div>'
+        )
+        self.assertEqual(rendered, html)
+
+        SimpleForm.bootstrap_options['form_layout'] = 'form-horizontal'
+        sform = SimpleForm()
+        rendered = sform.as_p()
+        html = (
+            u'<div class="form-group">'
+            u'<label class="control-label col-sm-2" for="id_choose">Choose:</label> '
+            u'<div class="col-sm-10">'
+            u'<select class="form-control" id="id_choose" name="choose">\n'
+            u'<option value="1">choice 1</option>\n'
+            u'<option value="2">choice 2</option>\n'
+            u'</select>'
+            u'</div>'
+            u'</div>'
         )
         self.assertEqual(rendered, html)
