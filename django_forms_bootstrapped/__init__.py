b2d3f056660f658723aafe8d51724651207d34fe
Bruno Miguel Silva <bmsilva@gmail.com>
2014-05-02 17:25:07 +0100

    Added widgets to global import.

--- django_forms_bootstrapped/__init__.py
+++ django_forms_bootstrapped/__init__.py
@@ -3,3 +3,7 @@ from .forms import Form
 from .fields import (
     CharField, ChoiceField, DateField, BooleanField, DateTimeField, FileField,
 )
+from .widgets import (
+    TextInput, DateInput, CheckboxInput, Select, Textarea, RadioSelect,
+    DateTimeInput, ClearableFileInput, PasswordInput,
+)
